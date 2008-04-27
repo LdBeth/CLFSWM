76c8d5ba245a23bfd52864c587a506f90a84b8d8
Philippe Brochard <pborchard@common-lisp.net>
2008-04-27 13:29:05 +0200

    populating

---
+++ src/clfswm-menu.lisp
@@ -0,0 +1,129 @@
+;;; --------------------------------------------------------------------------
+;;; CLFSWM - FullScreen Window Manager
+;;;
+;;; --------------------------------------------------------------------------
+;;; Documentation: Menu functions
+;;; --------------------------------------------------------------------------
+;;;
+;;; (C) 2005 Philippe Brochard <hocwp@free.fr>
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;;;
+;;; --------------------------------------------------------------------------
+
+(in-package :clfswm)
+
+
+(defstruct menu name item doc)
+(defstruct menu-item key value)
+
+
+(defvar *menu* (make-menu :name 'main))
+
+
+(defmacro with-all-menu ((menu item) &body body)
+  (let ((rec (gensym))
+	(subm (gensym)))
+    `(labels ((,rec (,item)
+		,@body
+		(when (menu-p ,item)
+		  (dolist (,subm (menu-item ,item))
+		    (,rec ,subm)))
+		(when (and (menu-item-p ,item) (menu-p (menu-item-value ,item)))
+		  (,rec (menu-item-value ,item)))))
+       (,rec ,menu))))
+
+(defun add-item (item &optional (menu *menu*))
+  (setf (menu-item menu) (nconc (menu-item menu) (list item))))
+
+(defun del-item (item &optional (menu *menu*))
+  (setf (menu-item menu) (remove item (menu-item menu))))
+
+
+
+;;; Finding functions
+(defun find-menu (name  &optional (root *menu*))
+  (with-all-menu (root item)
+    (when (and (menu-p item)
+	       (equal name (menu-name item)))
+	(return-from find-menu item))))
+
+
+(defun find-item-by-key (key &optional (root *menu*))
+  (with-all-menu (root item)
+    (when (and (menu-item-p item)
+	       (equal (menu-item-key item) key))
+      (return-from find-item-by-key item))))
+
+(defun find-item-by-value (value &optional (root *menu*))
+  (with-all-menu (root item)
+    (when (and (menu-item-p item)
+	       (equal (menu-item-value item) value))
+      (return-from find-item-by-value item))))
+
+
+(defun del-item-by-key (key &optional (menu *menu*))
+  (del-item (find-item-by-key key menu) menu))
+
+(defun del-item-by-value (value &optional (menu *menu*))
+  (del-item (find-item-by-value value menu) menu))
+
+
+
+;;; Convenient functions
+(defun add-menu-key (menu-name key value)
+  (add-item (make-menu-item :key key :value value) (find-menu menu-name)))
+
+(defun add-sub-menu (menu-name key sub-menu-name &optional (doc "Sub menu"))
+  (add-item (make-menu-item :key key :value (make-menu :name sub-menu-name :doc doc)) (find-menu menu-name)))
+
+
+(defun del-menu-key (menu-name key)
+  (del-item-by-key key (find-menu menu-name)))
+
+(defun del-menu-value (menu-name value)
+  (del-item-by-value value (find-menu menu-name)))
+
+(defun del-sub-menu (menu-name sub-menu-name)
+  (del-item-by-value (find-menu sub-menu-name) (find-menu menu-name)))
+
+
+
+
+;;; Display menu functions
+(defun open-menu (&optional (menu *menu*))
+  "Open the main menu"
+  (let ((info-list nil)
+	(action nil))
+    (dolist (item (menu-item menu))
+      (let ((value (menu-item-value item)))
+	(push (format nil "~A: ~A" (menu-item-key item) (typecase value
+							  (menu (format nil "< ~A >" (menu-doc value)))
+							  (t (documentation value 'function))))
+	      info-list)
+	(define-info-key-fun (list (menu-item-key item) 0)
+	    (lambda (&optional args)
+	      (declare (ignore args))
+	      (setf action value)
+	      (throw 'exit-info-loop nil)))))
+    (info-mode (nreverse info-list))
+    (dolist (item (menu-item menu))
+      (undefine-info-key-fun (list (menu-item-key item) 0)))
+    (typecase action
+      (menu (open-menu action))
+      (t (when (fboundp action)
+	   (funcall action))))))
+
+      
---
+++ src/netwm-util.lisp
@@ -0,0 +1,93 @@
+;;; --------------------------------------------------------------------------
+;;; CLFSWM - FullScreen Window Manager
+;;;
+;;; --------------------------------------------------------------------------
+;;; Documentation: NetWM functions
+;;;   http://freedesktop.org/wiki/Specifications_2fwm_2dspec
+;;; --------------------------------------------------------------------------
+;;;
+;;; (C) 2005 Philippe Brochard <hocwp@free.fr>
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;;;
+;;; --------------------------------------------------------------------------
+
+(in-package :clfswm)
+
+
+;;; Client List functions 
+(defun netwm-set-client-list (id-list)
+  (xlib:change-property *root* :_NET_CLIENT_LIST id-list :window 32))
+
+(defun netwm-get-client-list ()
+  (xlib:get-property *root* :_NET_CLIENT_LIST))
+
+(defun netwm-add-in-client-list (window)
+  (let ((last-list (netwm-get-client-list)))
+    (pushnew (xlib:window-id window) last-list)
+    (netwm-set-client-list last-list)))
+
+(defun netwm-remove-in-client-list (window)
+  (netwm-set-client-list (remove (xlib:window-id window) (netwm-get-client-list))))
+
+
+ 
+;;; Desktop functions ;; +PHIL
+(defun netwm-update-desktop-property ()
+  ;;  (xlib:change-property *root* :_NET_NUMBER_OF_DESKTOPS
+  ;;		   (list (length *workspace-list*)) :cardinal 32)
+  ;;  (xlib:change-property *root* :_NET_DESKTOP_GEOMETRY
+  ;;		   (list (xlib:screen-width *screen*)
+  ;;			 (xlib:screen-height *screen*))
+  ;;		   :cardinal 32)
+  ;;  (xlib:change-property *root* :_NET_DESKTOP_VIEWPORT
+  ;;		   (list 0 0) :cardinal 32)
+  ;;  (xlib:change-property *root* :_NET_CURRENT_DESKTOP
+  ;;		   (list 1) :cardinal 32)
+;;; TODO
+  ;;(xlib:change-property *root* :_NET_DESKTOP_NAMES
+  ;;		   (list "toto" "klm" "poi") :string 8 :transform #'xlib:char->card8))
+  )
+
+
+
+
+;;; Taken from stumpwm (thanks)
+(defun netwm-set-properties ()
+  "Set NETWM properties on the root window of the specified screen.
+FOCUS-WINDOW is an extra window used for _NET_SUPPORTING_WM_CHECK."
+  ;; _NET_SUPPORTED
+  (xlib:change-property *root* :_NET_SUPPORTED
+			(mapcar (lambda (a)
+				  (xlib:intern-atom *display* a))
+				(append +netwm-supported+
+					(mapcar 'car +netwm-window-types+)))
+			:atom 32)
+  ;; _NET_SUPPORTING_WM_CHECK
+  (xlib:change-property *root* :_NET_SUPPORTING_WM_CHECK
+			(list *no-focus-window*) :window 32
+			:transform #'xlib:drawable-id)
+  (xlib:change-property *no-focus-window* :_NET_SUPPORTING_WM_CHECK
+			(list *no-focus-window*) :window 32
+			:transform #'xlib:drawable-id)
+  (xlib:change-property *no-focus-window* :_NET_WM_NAME
+			"clfswm"
+			:string 8 :transform #'xlib:char->card8)
+  (netwm-update-desktop-property))
+
+
+
+
+
