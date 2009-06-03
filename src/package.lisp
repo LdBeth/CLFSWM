093c522ba01a62655322ea37a5f8a9dd1204f4d5
Philippe Brochard <pbrochard@common-lisp.net>
2009-06-03 23:51:23 +0200

    fixe TODO and bannish pointer placement

--- src/package.lisp
+++ src/package.lisp
@@ -252,7 +252,7 @@ It is particulary useful with CLISP/MIT-CLX.")
 ;;; middle-left  middle-middle  middle-right
 ;;; bottom-left  bottom-middle  bottom-right
 ;;;
-(defparameter *banish-pointer-placement* 'bottom-left-child-placement)
+(defparameter *banish-pointer-placement* 'bottom-right-child-placement)
 (defparameter *second-mode-placement* 'top-middle-child-placement)
 (defparameter *info-mode-placement* 'top-left-child-placement)
 (defparameter *query-mode-placement* 'top-left-child-placement)
