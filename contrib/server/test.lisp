3255fab7d3e5b0b6c4329acc9c1b9ec083182494
Philippe Brochard <pbrochard@common-lisp.net>
2010-10-27 22:18:42 +0200

    contrib/server: add a test file

---
+++ contrib/server/test.lisp
@@ -0,0 +1,15 @@
+(in-package :clfswm)
+
+(leave-frame)
+(select-previous-level)
+
+(let ((frame (create-frame \:name \"Test root\" \:x 0.05 \:y 0.05)))
+  (add-frame frame *current-child*)
+  (add-frame (create-frame \:name \"Test 1\" \:x 0.3 \:y 0 \:w 0.7 \:h 1) frame)
+  (add-frame (create-frame \:name \"Test 2\" \:x 0 \:y 0 \:w 0.3 \:h 1) frame)
+  (setf *current-child* (first (frame-child frame))))
+
+(show-all-children *current-root*)
+
+
+
