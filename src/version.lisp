c651d433ec5f91f5d3a89d00a679099c89bf8043
Philippe Brochard <pbrochard@common-lisp.net>
2011-06-04 14:37:27 +0200

    Version update

--- src/version.lisp
+++ src/version.lisp
@@ -33,4 +33,4 @@
 
 (in-package :version)
 
-(defparameter *version* #.(concatenate 'string "Version: 1103   built " (date-string)))
+(defparameter *version* #.(concatenate 'string "Version: 1106   built " (date-string)))
