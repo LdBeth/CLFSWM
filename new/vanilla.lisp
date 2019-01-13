35759a53ac056e7eca3e7d97c3ca45c069d54a9d
LdBeth <andpuke@foxmail.com>
2019-01-13 00:45:27 -0800

    revise self desc message

--- new/vanilla.lisp
+++ new/vanilla.lisp
@@ -5,10 +5,16 @@
 (defflavor vanilla () () :no-vanilla-flavor)
 
 (defmethod (vanilla :describe) ()
-  (describe-flavor (slot-value cl::self 'class-name)))
+  (format t "~a, an object of flavor ~a,
+ has instance variable values:~%"
+          cl::self
+          (flavor-instance-class-name cl::self))
+  (dolist (item (flavor-instance-vars cl::self))
+    (format t " ~s   ~s~%" (car item) (cdr item))))
 
 (defmethod (vanilla :print-self) (&optional (stream t) (depth 1))
   (declare (ignore depth))
   (format stream "#<~a ~d>"
           (flavor-instance-class-name cl::self)
           (flavor-instance-unique-number cl::self)))
+
