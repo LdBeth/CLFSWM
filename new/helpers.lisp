54fc912246b9e6b47c74fd1e3278367659d712b2
LdBeth <andpuke@foxmail.com>
2019-01-13 01:08:37 -0800

    fix sbcl

--- new/helpers.lisp
+++ new/helpers.lisp
@@ -1,5 +1,6 @@
 (in-package #:flavors)
 
 (defun parse-body (body)
-  #+asdf (uiop:parse-body body :documentation nil)
-  #-asdf (error "Don't have UIOP"))
+  #+asdf3.2 (uiop:parse-body body :documentation nil)
+  #(sbcl) (sb-int:parse-body body nil t)
+  #-(or asdf3.2 sbcl) (error "Don't have UIOP"))
